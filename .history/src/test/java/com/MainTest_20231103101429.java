01d7735caf283f3a318358ae9f9151ef6df9664a
Diana <1636715789@qq.com>
2023-11-03 14:50:40 +0800

    添加测试用例后的更改

--- .history/src/test/java/com/MainTest_20231103101429.java
+++ .history/src/test/java/com/MainTest_20231103101429.java
@@ -10,10 +10,7 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		int result2 = main.add(	3,4);
 		assertEquals(2, result);
-		assertEquals(7, result2);
-
 	}
 
 }
